848df686944b2219a15fb1daf05c701541ea05af
volkan filazi <volkanfilazi@hotmail.com>
2023-07-02 14:47:35 +0200

    added darkmode theme and value added inside localstorage

--- src/app/navbar/navbar.components.ts
+++ src/app/navbar/navbar.components.ts
@@ -1,6 +1,4 @@
-import { Component } from "@angular/core";
-import { Icon } from '@iconify/vue';
-
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +6,10 @@ import { Icon } from '@iconify/vue';
 })
 
 export class NavbarComponent {
+  @Input() darkTheme: boolean | undefined  
+  @Output() darkThemeToggled = new EventEmitter<void>();
 
+  toggleDarkTheme() {
+    this.darkThemeToggled.emit();
+  }
 }
