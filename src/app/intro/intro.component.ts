3035375d82ccbd4d7a78536425bd18c25f06c142
volkan filazi <volkanfilazi@hotmail.com>
2023-07-02 17:22:37 +0200

    added intro animation

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,20 +1,54 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
 })
-export class IntroComponent implements OnInit {
-  intro:string = "Intro";
-  outputChildMessage:string = 'Message from chil component to parent';
-
-  @Input() fromParent:string | undefined;
-  @Input() fromParent2:string | undefined;
-  @Input() darkTheme: boolean | undefined
-
+export class IntroComponent implements OnChanges, OnDestroy {
+  intro: string = "Intro";
+  @Input() darkTheme: boolean | undefined;
   @Output() messageEvent = new EventEmitter<string>();
+  displayText: string = '';
+
+  private timer: any; 
+
   ngOnInit(): void {
-      
+    this.introAnimation();
   }
-  
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['darkTheme'] && !changes['darkTheme'].firstChange) {
+      this.introAnimation();
+    }
+  }
+
+  introAnimation() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+
+    let animationText = 'Creative, patient and innovative';
+    let i = 0;
+    let lastIndex = 3;
+    this.timer = setInterval(() => {
+      if (i < animationText.length) {
+        this.displayText = animationText.substring(0, i + 1);
+        lastIndex = i;
+      } else if (lastIndex > 0) {
+        this.displayText = animationText.substring(0, lastIndex);
+        lastIndex--;
+      } else {
+        this.displayText = '';
+        i = 0;
+        lastIndex = 0;
+      }
+      i++;
+    }, 200);
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
 }
