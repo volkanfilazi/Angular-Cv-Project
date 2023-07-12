56b40954e17d784495c86cdea53909ae330645e2
volkan filazi <volkanfilazi@hotmail.com>
2023-07-12 23:08:19 +0200

    added ionic android mobile version

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.volkanfilaziresume.app',
+  appName: 'volkanfilazi-resume',
+  webDir: 'dist/angular-starting',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
